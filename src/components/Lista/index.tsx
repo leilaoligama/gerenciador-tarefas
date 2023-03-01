5fc7cb289fe8ce9958bf16278620043ab7d4b2bf
Leila <leilagama@gmail.com>
2023-03-01 17:52:50 -0300

    criação de states

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-  const tarefas = [{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }];
-  
+  const [tarefas, setTarefas] = useState(
+    [
+      {
+      tarefa: 'React',
+      tempo: '02:00:00'
+      }, 
+      {
+        tarefa: 'JavaScript',
+        tempo: '01:00:00'
+      },
+      { 
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+      }
+    ]
+  )
+
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick=
+        { 
+          function() {
+            console.log("h2 clicado: ", tarefas);
+            setTarefas(
+              [
+                ...tarefas, /*param1: um spread de tarefas*/
+                { tarefa: "Estudando estado", tempo: "05:00:00" } //param 2: um objeto tarefa
+              ] 
+            )
+          }
+        }
+      >
+         Estudos do dia 
+      </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item key={index}
