75bf993a33122198d2679beec9b05a6c1daa763c
Leila <leilagama@gmail.com>
2023-03-06 17:11:16 -0300

    Finalização da aplicação

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,12 +1,35 @@
-import style from '../Lista.module.scss';
+import { ITarefa } from '../../../types/tarefa';
+import style from './Item.module.scss';
 
-export default function Item(props: {tarefa: string, tempo: string}){
-    const {tarefa, tempo} = props;
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
 
-    return(
-        <li className={style.item}>
-            <h3>{tarefa}</h3>
-            <span>{tempo}</span>
-        </li>
-    )
+export default function Item(
+  {
+    tarefa,
+    tempo,
+    selecionado,
+    completado,
+    id,
+    selecionaTarefa
+  }: Props) {
+  return (
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`}
+      onClick={() => !completado && selecionaTarefa(
+        {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        }
+        )}
+      >
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+      {completado && <span className={style.concluido} aria-label="tarefa completada"></span>}
+    </li>
+  )
 }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,37 +1,29 @@
+import React from 'react';
 import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista({ tarefas }: { tarefas: ITarefa[]}) {
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
+
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
   return (
     <aside className={style.listaTarefas}>
-      <h2
-        // { 
-        //   () => {
-        //     setTarefas(
-        //       [
-        //         ...tarefas, /*param1: um spread de tarefas*/
-        //         { tarefa: "Estudando estado", tempo: "01:00:00" } //param 2: um objeto tarefa
-        //       ] 
-        //     )
-        //   }
-        // }
-        //onClick={() => setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00" }])}
-      >
-         Estudos do dia 
-      </h2>
+      <h2> Estudos do dia </h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index}
-            tarefa={item.tarefa}
-            tempo={item.tempo}
-            // ou
-            // {...item} //importa todas as propriedades do objeto.
+        {tarefas.map(item => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
           />
         ))}
       </ul>
     </aside>
-  );
+  )
 }
 
 export default Lista;
